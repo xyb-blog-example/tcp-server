e16a164978e933e7cef554f2bcae7d1d9dbaf4cc
wangzhengjun <wangzhengjun@didichuxing.com>
2018-10-29 22:39:52 +0800

    提交代码

--- v1/client/init.go
+++ v1/client/init.go
@@ -8,23 +8,30 @@ import (
 
 func CreateClient() {
 	//1 创建待连接的远程节点
-	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8000")
+	tcpRAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8000")
 	if err != nil {
 		fmt.Println("创建TCP远程节点失败,error:" + err.Error())
 		return
 	}
 
-	//2 连接远程节点
-	tcpConn, err := net.DialTCP("tcp", nil, tcpAddr)
+	//2 创建待连接的本地节点
+	tcpLAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8001")
+	if err != nil {
+		fmt.Println("创建TCP远程节点失败,error:" + err.Error())
+		return
+	}
+
+	//3 连接远程节点
+	tcpConn, err := net.DialTCP("tcp", tcpLAddr, tcpRAddr)
 	if err != nil {
 		fmt.Println("连接服务端失败,error:" + err.Error())
 		return
 	}
 
-	//3 启动goroutine，处理服务端的输入数据
+	//4 启动goroutine，处理服务端的输入数据
 	go handleServer(tcpConn)
 
-	//4 客户端先向服务端发起发一点数据，然后服务端进行相应
+	//5 客户端先向服务端发起发一点数据，然后服务端进行相应
 	for i := 0; i < 10; i++ {
 		text := fmt.Sprintf("%d + %d = ?", i, i)
 		fmt.Println("向服务端发送了以下数据:", text)
